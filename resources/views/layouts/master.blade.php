62e60a626fb69196c78b18585090f41761596c00
Stanislav Zdravkov <stanislav.zdravkov@intercard.bg>
2019-03-01 17:36:39 +0200

    First commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+</head>
+<body>
+@include('partials.header')
+<div class="container">
+    @yield('content')
+</div>
+</body>
+</html>
