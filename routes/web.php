75ad43651d5357e73a23ac42a8101055598edece
slavizdravkov <slavizdravkov@gmail.com>
2019-03-05 22:15:18 +0200

    Protect routes with auth middleware

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::get('about', function () {
     return view('other.about');
 })->name('other.about');
 
-Route::group(['prefix' => 'admin'], function () {
+Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function () {
     Route::get('', [
         'uses' => 'PostController@getAdminIndex',
         'as' => 'admin.index'
